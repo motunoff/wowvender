b0659734380d43f9a8e02a9c300b5604243422e4
motunoff <motunov7778@ya.ru>
2020-03-07 17:24:14 +0300

    code review

--- index.php
+++ index.php
@@ -606,7 +606,7 @@
 					echo "<div class='users-table-row flex justify-content-center'>";
 					echo "<div class='user-name'>$row[username]</div>";
 
-					$user_role = mysqli_query($con, "SELECT rolename FROM user_role  WHERE id	='$row[role_id]' limit 1 ");
+					$user_role = mysqli_query($con, "SELECT rolename FROM user_role  WHERE id='$row[role_id]' limit 1 ");
 
 					if ( $user_role->num_rows > 0 ) {
 
